961d0827c03d3a9b7d984cf651137cb6bdcd14a5
Ramy Bachayev <90751345+ramyb3@users.noreply.github.com>
2022-03-26 17:29:36 +0300

    Add files via upload

---
+++ Project/src/App.js
@@ -0,0 +1,124 @@
+import RouteComp, { getData } from './models/route';
+import './App.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {  faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+function App()
+{
+    const storeData = useSelector(state => state);
+    const dispatch = useDispatch();
+
+    const[theme,setTheme]= useState(true); // default - light theme
+    
+    const colorPageButton= ['rgb(123, 185, 171)','white']; // button color of this page
+
+    useEffect(()=> 
+    {
+        // default button page will be colored
+        document.getElementById('home').style.backgroundColor= colorPageButton[0];
+        document.getElementById('home').style.color= colorPageButton[1];
+
+        // check permission to user location
+        window.navigator.permissions && window.navigator.permissions.query({name: 'geolocation'})
+        .then(async function(PermissionStatus) 
+        {
+            if (PermissionStatus.state == 'granted') 
+            {
+                // weather in this location
+                window.navigator.geolocation.getCurrentPosition(async(position) => {
+
+                    try
+                    {
+                        let data= await getData([position.coords.latitude,position.coords.longitude],4);
+                        let today= await getData(data[1],1);
+                        let days= await getData(data[1],2);
+
+                        dispatch({type: "LOAD", payload: [today,days,data]});
+                    }
+
+                    catch(e)
+                    {
+                        alert('An error has occurred! - check developer tools');
+                        console.log(e);
+                    }
+                }); 
+            }
+
+            else // weather in default location - Tel Aviv
+            {
+                try
+                {
+                    let data= ['Tel Aviv', 215854];
+                    let today= await getData(data[1],1);
+                    let days= await getData(data[1],2);
+
+                    dispatch({type: "LOAD", payload: [today,days,data]});
+                }
+
+                catch(e)
+                {
+                    alert('An error has occurred! - check developer tools');
+                    console.log(e);
+                }
+            }
+        })
+    },[]); //when app start
+    
+    useEffect(()=> 
+    {
+        if(theme==true)
+        {
+            document.body.classList.add("light");
+            document.body.classList.remove("dark");
+        }
+
+        else
+        {
+            document.body.classList.add("dark");
+            document.body.classList.remove("light");
+        }
+    },[theme]); //every time the theme changes
+
+    const color=(e)=> // this button page will be colored
+    {
+        document.getElementById('home').style='';
+        document.getElementById('fav').style='';
+
+        e.style.backgroundColor= colorPageButton[0];
+        e.style.color= colorPageButton[1];
+    }
+
+    return (<>
+    
+        <header>
+            <div>&nbsp;Weather App</div>
+
+            <div>  
+                <Link to='/'><input id="home" type="button" value="Home" onClick={e=> color(e.target)}/></Link>&nbsp;
+                <Link to='favorites'><input id="fav" type="button" value="Favorites" onClick={e=> color(e.target)}/></Link>&nbsp; 
+            </div>
+        </header><br/>
+
+        <div style={{textAlign: 'center'}}>
+
+            {/* button - shows Fahrenheit or Celsius */}
+            <input type='button' value='C/F' onClick={()=> dispatch({type: "TEMP", payload: !storeData[1]})}/>&nbsp;
+        
+            <button id='bt' onClick={()=> setTheme(!theme)}>
+                {theme==false ? // dark or light theme
+                    <FontAwesomeIcon icon={faSun} className='sun'/>
+                    :    
+                    <FontAwesomeIcon icon={faMoon} className='moon'/>
+                }
+            </button>
+        </div>
+
+        <RouteComp/>
+    </>)
+}
+
+export default App;
